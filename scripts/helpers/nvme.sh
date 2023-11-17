30089efa76320fe4d85c4d3f957b6ce48baf8e36
Jordan Jacobelli <jjacobelli@nvidia.com>
2023-11-17 11:26:23 +0100

    Make NVME service a valid OneShot systemd service
    Signed-off-by: Jordan Jacobelli <jjacobelli@nvidia.com>

--- scripts/helpers/nvme.sh
+++ scripts/helpers/nvme.sh
@@ -3,7 +3,8 @@ set -euo pipefail
 
 if [ -e "/dev/nvme1n1" ] && [ "${NV_RUNNER_ENV}" = "aws" ]; then
   echo "nvme drive found. mounting..."
-  mkfs -t xfs /dev/nvme1n1
+  mkfs -t xfs -f /dev/nvme1n1
+  rm -rf /data
   mkdir /data
   mount /dev/nvme1n1 /data
 else
