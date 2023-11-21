2fb3db1c08944fb0ae6ec1367c43009ebcc79fc9
AJ Schmidt <aschmidt@nvidia.com>
2023-11-21 15:44:01 -0500

    update matrix/config value handling since `yq` and `jq` are available in GitHub-hosted runners that do not use containers

--- ci/compute-matrix.sh
+++ ci/compute-matrix.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 set -euo pipefail
 
-# This script computes the matrix for the GHAs matrix job. The values in
-# config.yaml are injected into each matrix entry according to its `ENV` key.
+# This script computes the matrix for the GHAs matrix job.
 
 yq -o json \
-  eval-all \
-  '{"matrix": select(filename == "matrix.yaml")} + {"config": select(filename == "config.yaml")}' \
-  matrix.yaml config.yaml | \
-  jq -c 'include "ci/compute-matrix"; compute_matrix(.matrix; .config)'
+  '.' \
+  matrix.yaml | \
+  jq -c 'include "ci/compute-matrix"; compute_matrix(.)'
