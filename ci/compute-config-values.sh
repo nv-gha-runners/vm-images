14a10dd8c73924d3cce070135a6f6643594866f4
AJ Schmidt <aschmidt@nvidia.com>
2023-11-22 09:59:25 -0500

    ensure CI scripts have comments after shebang

--- ci/compute-config-values.sh
+++ ci/compute-config-values.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-set -euo pipefail
-
 # This script outputs the values in config.yaml for the environment specified by
 # the `RUNNER_ENV` environment variable. The output is in the form of
 # `KEY=VALUE` pairs, one per line. This allows the output to be injected as
@@ -8,6 +6,8 @@ set -euo pipefail
 # uppercases the key. Here is an example when `RUNNER_ENV=aws`:
 # NV_DOMAIN=arc-eks
 # NV_PACKER_SOURCE=amazon-ebs
+set -euo pipefail
+
 
 yq -o json \
   '.[env(RUNNER_ENV)]' \
