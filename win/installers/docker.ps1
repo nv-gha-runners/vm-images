c383088b30aa1c9d5ee93aa6a86c57bd3f9d2923
AJ Schmidt <aschmidt@nvidia.com>
2024-01-23 17:47:02 -0500

    update some syntax on installers: add `env:` and `{}`

--- win/installers/docker.ps1
+++ win/installers/docker.ps1
@@ -2,21 +2,21 @@ $ProgressPreference = "SilentlyContinue"
 $ErrorActionPreference = "Stop"
 $root = "C:/docker"
 
-. "$NV_CONTEXT_DIR/envvars.ps1"
+. "${env:NV_CONTEXT_DIR}/envvars.ps1"
 
 Stop-Service docker -ErrorAction Ignore
-Remove-Item -Recurse -Force -Path $root -ErrorAction Ignore
-$nul = mkdir -Force $root -ErrorAction Ignore
+Remove-Item -Recurse -Force -Path "${root}" -ErrorAction Ignore
+$nul = mkdir -Force "${root}" -ErrorAction Ignore
 
 Enable-WindowsOptionalFeature -NoRestart -Online -FeatureName Containers
 
-Set-Location $root
+Set-Location "${root}"
 
 # TODO: Get latest release version from `moby/moby` repository. Requires adapting `linux/context/github.sh` for Windows.
-$Uri = "https://download.docker.com/win/static/stable/x86_64/docker-25.0.0.zip"
-Invoke-WebRequest -UseBasicParsing -OutFile docker.zip -Uri $Uri
+$uri = "https://download.docker.com/win/static/stable/x86_64/docker-25.0.0.zip"
+Invoke-WebRequest -UseBasicParsing -OutFile docker.zip -Uri "${uri}"
 tar --strip-components=1 -xf docker.zip
 Remove-Item docker.zip
 
 ./dockerd.exe --register-service
-Set-MachineEnvironmentVariable -Append -Variable "Path" -Value "$root"
+Set-MachineEnvironmentVariable -Append -Variable "Path" -Value "${root}"
