c119285b0129b5c256f868e13a38f2ad9b025745
Wesley Maxey <wesley.maxey@gmail.com>
2024-02-02 12:08:47 -0800

    Delete environment.ps1.

--- win/installers/environment.ps1
+++
@@ -1,9 +0,0 @@
-. "${env:NV_CONTEXT_DIR}/envvars.ps1"
-
-mkdir -Force -ErrorAction SilentlyContinue "${env:NV_EXE_DIR}" | Out-Null
-mkdir -Force -ErrorAction SilentlyContinue "C:/docker"         | Out-Null
-
-Set-MachineEnvironmentVariable -Append -Variable "Path" -Value "${env:NV_EXE_DIR}"
-Set-MachineEnvironmentVariable -Append -Variable "Path" -Value "C:/docker"
-
-Enable-WindowsOptionalFeature -NoRestart -Online -FeatureName Containers
