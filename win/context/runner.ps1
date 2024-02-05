71aeb955b9235eca9f9fb73dc67ece7cbf8fe194
Wesley Maxey <wesley.maxey@gmail.com>
2024-02-05 11:20:53 -0800

    make a polling loop for the runner.

--- win/context/runner.ps1
+++ win/context/runner.ps1
@@ -1,13 +1,27 @@
 $ProgressPreference = "SilentlyContinue"
 $ErrorActionPreference = "Stop"
 
-# TODO: switch this to a `for` loop to poll for `C:/jitconfig` file
-# Briefly wait to start the runner
-Start-Sleep -Seconds 10
+$jitconfig = "C:/jitconfig"
+$maxPollCount = 20
+
+for ($count = 0; $count -lt $maxPollCount; $count++) {
+    Write-Output "waiting for jitconfig ($count / $maxPollCount)"
+    Start-Sleep -Seconds 10
+    $exists = Test-Path $jitconfig
+
+    if ($exists) {
+        break
+    }
+}
+
+if (-not $exists) {
+    Stop-Computer -Force & # background job so that we can exit after writing an error.
+    Write-Error "$jitconfig not found after 200 seconds, exiting"
+}
 
 # Setup runner hook and config
 Write-Output "Trying to fetch JITConfig"
-$ENV:ACTIONS_RUNNER_INPUT_JITCONFIG = Get-Content -Path "C:/jitconfig" -ErrorAction Stop
+$ENV:ACTIONS_RUNNER_INPUT_JITCONFIG = Get-Content -Path $jitconfig -ErrorAction Stop
 $ENV:ACTIONS_RUNNER_HOOK_JOB_STARTED = "C:/actions-runner/.initialize_runner.sh"
 
 # Remove runner config
