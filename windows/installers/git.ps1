c18171bba7c6184b859b2a477d82bee0c6da2cef
Paul Taylor <178183+trxcllnt@users.noreply.github.com>
2024-02-16 01:17:04 -0800

    Add git to PATH in Windows images

--- windows/installers/git.ps1
+++ windows/installers/git.ps1
@@ -13,5 +13,8 @@ Invoke-WebRequest -UseBasicParsing -Uri "${gitUri}" -OutFile "./git_setup.exe"
 # Use Start-Process to wait for installer to finish completely
 Start-Process -Wait -FilePath "./git_setup.exe" -ArgumentList "/SP- /VERYSILENT /NORESTART /NOCLOSEAPPLICATIONS"
 
+# For bash.exe, add 'C:\Program Files\Git\bin' to PATH
+[Environment]::SetEnvironmentVariable('Path', "$([Environment]::GetEnvironmentVariable('Path', 'Machine'));C:\Program Files\Git\bin", 'Machine')
+
 # Cleanup
 Remove-Item "./git_setup.exe"
