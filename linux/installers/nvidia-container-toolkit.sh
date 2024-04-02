cf43be35a3e25d444253ef572812bf4859b7f760
Jordan Jacobelli <jjacobelli@nvidia.com>
2024-04-02 14:36:00 +0200

    Use CDI for GPU containers
    Signed-off-by: Jordan Jacobelli <jjacobelli@nvidia.com>

--- linux/installers/nvidia-container-toolkit.sh
+++ linux/installers/nvidia-container-toolkit.sh
@@ -17,7 +17,7 @@ curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-contai
 
 sudo apt-get update
 
-sudo apt-get install -y --no-install-recommends nvidia-container-toolkit
+sudo apt-get install -y --no-install-recommends nvidia-container-toolkit-base
 
 sudo rm -rf "${APT}" "${KEYRING}"
 
@@ -25,6 +25,12 @@ sudo rm -rf "${APT}" "${KEYRING}"
 DOMAIN=$(yq '.[env(NV_RUNNER_ENV)].domain' "${NV_CONTEXT_DIR}/config.yaml")
 export DOMAIN
 envsubst < "${NV_CONTEXT_DIR}/dockerd.gpu.json" | sudo tee /etc/docker/daemon.json
+
+# Enable CDI
+sudo nvidia-ctk config --in-place --set nvidia-container-runtime.mode=cdi
+
+# Add udev rule to generate CDI spec at boot
+sudo cp "${NV_CONTEXT_DIR}/nvidia-cdi.rules" /lib/udev/rules.d/71-nvidia-cdi.rules
 
 sudo systemctl restart docker
 docker info
