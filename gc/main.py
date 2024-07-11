83357c2d5b6576de786333fc34862eaf77f0076a
AJ Schmidt <aschmidt@nvidia.com>
2024-07-10 21:26:32 -0400

    add multi-region support for garbage collection scripts

--- gc/main.py
+++ gc/main.py
@@ -1,6 +1,8 @@
 import argparse
 import subprocess
 import json
+import yaml
+from os import path
 from collectors.ecr import ECRGarbageCollector
 from collectors.amis import AMIGarbageCollector
 from collectors.gc import GarbageCollector
@@ -10,14 +12,20 @@ def load_current_images() -> list[str]:
     """
     Loads the currently supported images from the matrix and returns them as a list.
     """
+    compute_matrix_path = path.join(
+        path.dirname(__file__), "..", "ci", "compute-matrix.sh"
+    )
+    compute_image_name_path = path.join(
+        path.dirname(__file__), "..", "ci", "compute-image-name.sh"
+    )
     result = subprocess.run(
-        "ci/compute-matrix.sh", cwd="..", capture_output=True, check=True
+        compute_matrix_path, cwd="..", capture_output=True, check=True
     )
     matrix = json.loads(result.stdout.decode("utf-8"))["include"]
     images = []
     for entry in matrix:
         result = subprocess.run(
-            "ci/compute-image-name.sh",
+            compute_image_name_path,
             cwd="..",
             capture_output=True,
             env=entry,
@@ -44,7 +52,27 @@ if __name__ == "__main__":
         print("No current images found. Something's not right.")
         print("Exiting to prevent all images from being deleted from AWS.")
         exit(1)
-    for GC in [ECRGarbageCollector, AMIGarbageCollector]:
-        gc: GarbageCollector = GC(current_images, dry_run)
+
+    regions_path = path.join(path.dirname(__file__), "..", "regions.yaml")
+    with open(regions_path, "r") as file:
+        regions = yaml.safe_load(file)
+
+    ecr_collectors = [
+        ECRGarbageCollector(
+            current_images=current_images,
+            region=regions["public_ecr_region"],
+            dry_run=dry_run,
+        )
+    ]
+    ami_collectors = [
+        AMIGarbageCollector(
+            current_images=current_images,
+            region=region,
+            dry_run=dry_run,
+        )
+        for region in [regions["default_region"]] + regions["backup_regions"]
+    ]
+
+    for gc in ecr_collectors + ami_collectors:
         gc.run()
         print()
