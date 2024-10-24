765c22562f719e38328a4553716fb158fc7880a1
Kyle Edwards <kyedwards@nvidia.com>
2024-10-24 14:32:40 -0400

    shellcheck

--- ci/image-name/deserialize.sh
+++ ci/image-name/deserialize.sh
@@ -2,6 +2,6 @@
 
 set -euo pipefail
 
-cd "$(dirname $0)/../.."
+cd "$(dirname "$0")/../.."
 
 MATRIX="$(yq -o json matrix.yaml)" IMAGE_NAME="$1" jq -c -n 'include "ci/image-name/deserialize"; env.IMAGE_NAME | deserialize_image_name'
