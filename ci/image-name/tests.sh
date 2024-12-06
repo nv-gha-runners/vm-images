03c7ebe295ab7442b8428def0c687ee93692f87a
Kyle Edwards <kyedwards@nvidia.com>
2024-12-06 09:48:11 -0500

    Fix test

--- ci/image-name/tests.sh
+++ ci/image-name/tests.sh
@@ -5,4 +5,4 @@ set -euo pipefail
 MATRIX="$(yq -o json matrix.yaml)"
 MATRIX="${MATRIX}" jq --run-tests ci/image-name/deserialize.test
 
-ci/image-name/deserialize.sh linux-gpu-550-open-amd64-pr-1234 | jq -e '. == {"os": "linux", "variant": "gpu", "driver_version": "550", "driver_flavor": "open", "arch": "amd64", "branch_name": "pr-1234"}'
+ci/image-name/deserialize.sh linux-gpu-565-open-amd64-pr-1234 | jq -e '. == {"os": "linux", "variant": "gpu", "driver_version": "565", "driver_flavor": "open", "arch": "amd64", "branch_name": "pr-1234"}'
