2a0278175781c96c3762fcff74ee31c4e571ccbf
Kyle Edwards <kyedwards@nvidia.com>
2024-12-06 10:17:16 -0500

    Refactor tests to always get latest driver version

--- ci/image-name/tests.sh
+++ ci/image-name/tests.sh
@@ -3,6 +3,38 @@
 set -euo pipefail
 
 MATRIX="$(yq -o json matrix.yaml)"
-MATRIX="${MATRIX}" jq --run-tests ci/image-name/deserialize.test
+LATEST_DRIVER_VERSION="$(echo "${MATRIX}" | jq -r '.DRIVER_VERSION[-1]')"
 
-ci/image-name/deserialize.sh linux-gpu-565-open-amd64-pr-1234 | jq -e '. == {"os": "linux", "variant": "gpu", "driver_version": "565", "driver_flavor": "open", "arch": "amd64", "branch_name": "pr-1234"}'
+ci/image-name/deserialize.sh "linux-gpu-${LATEST_DRIVER_VERSION}-open-arm64-main" | \
+  LATEST_DRIVER_VERSION="${LATEST_DRIVER_VERSION}" \
+  jq -e ". == {
+    \"os\": \"linux\",
+    \"variant\": \"gpu\",
+    \"driver_version\": env.LATEST_DRIVER_VERSION,
+    \"driver_flavor\": \"open\",
+    \"arch\": \"arm64\",
+    \"branch_name\": \"main\"
+  }"
+ci/image-name/deserialize.sh "windows-cpu-amd64-pr-1234" | \
+  jq -e ". == {
+    \"os\": \"windows\",
+    \"variant\": \"cpu\",
+    \"driver_version\": null,
+    \"driver_flavor\": null,
+    \"arch\": \"amd64\",
+    \"branch_name\": \"pr-1234\"
+  }"
+ci/image-name/deserialize.sh "linux-gpu-${LATEST_DRIVER_VERSION}-open-arm64" | \
+  LATEST_DRIVER_VERSION="${LATEST_DRIVER_VERSION}" \
+  jq -e ". == {
+    \"os\": \"linux\",
+    \"variant\": \"gpu\",
+    \"driver_version\": env.LATEST_DRIVER_VERSION,
+    \"driver_flavor\": \"open\",
+    \"arch\": \"arm64\",
+    \"branch_name\": null
+  }"
+
+! ci/image-name/deserialize.sh "linux-${LATEST_DRIVER_VERSION}-arm64-main"
+! ci/image-name/deserialize.sh "gpu-${LATEST_DRIVER_VERSION}-arm64-main"
+! ci/image-name/deserialize.sh "linux-gpu-${LATEST_DRIVER_VERSION}-main"
